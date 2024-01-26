76aa0babd892b2d26c09afce3e3858e2c70d71ff
Andre Geisler <andre.geisler@keysight.com>
2024-01-26 14:46:08 +0100

    add vertex2d_rotate

--- library/src/vertex2d.c
+++ library/src/vertex2d.c
@@ -4,6 +4,8 @@
 
 #include <exdevgfx/vertex2d.h>
 
+#include <math.h>
+
 void vertex2d_init(Vertex2d_t v) {
     v[0] = 0;
     v[1] = 0;
@@ -21,4 +23,11 @@ void vertex2d_copy(const Vertex2d_t src, Vertex2d_t dst) {
 
 float vertex2d_cross(const Vertex2d_t a, const Vertex2d_t b) {
     return a[0] * b[1] - a[1] * b[0];
+}
+
+void vertex2d_rotate(const Vertex2d_t v, Vertex2d_t d, const float a) {
+    const float s = sin(a);
+    const float c = cos(a);
+    d[0] = v[0] * c - v[1] * s;
+    d[1] = v[1] * c + v[0] * s;
 }
