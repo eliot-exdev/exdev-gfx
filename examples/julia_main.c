c277f6851880ae1f410c0ca56afe96a122979119
Andre Geisler <andre@exdev.de>
2024-10-13 10:08:47 +0200

    minor changes in julia_main.c

--- examples/julia_main.c
+++ examples/julia_main.c
@@ -7,17 +7,17 @@
 #include <stdlib.h>
 
 #define EXDEVGFX2_LOG_LEVEL 2
+
 #include <exdevgfx/logger.h>
 
-int width = 320;
-int height = 240;
-int max_it = 50;
+static int width = 640;
+static int height = 480;
+static int max_it = 50;
 
 #define IMAGE_PATH "julia.ppm"
 
-static void print_help()
-{
-    printf("mandelbrot  [OPTIONS]...\n"
+static void print_help() {
+    printf("julia  [OPTIONS]...\n"
            "options:\n"
            " -h  --help              print help message and exit\n"
            "     --width <par>       the width of the window (default 320)\n"
@@ -30,35 +30,29 @@ static void print_help()
            "     -                          zoom out\n"
            "     s                          save current view\n"
            "     r                          reset view\n"
-           "     cursor up/down/left/right  navigate\n"    );
+           "     cursor up/down/left/right  navigate\n");
 }
 
-static void parse_args(int argc, char **argv)
-{
-    if (args_find_option(argc, argv,'h', "help"))
-    {
+static void parse_args(const int argc, char **argv) {
+    if (args_find_option(argc, argv, 'h', "help")) {
         print_help();
         exit(0);
     }
     const char *ptr = NULL;
-    if ((ptr = args_get_long_option_parameter(argc, argv, "width")) != NULL)
-    {
+    if ((ptr = args_get_long_option_parameter(argc, argv, "width")) != NULL) {
         width = atoi(ptr);
     }
-    if ((ptr = args_get_long_option_parameter(argc, argv, "height")) != NULL)
-    {
+    if ((ptr = args_get_long_option_parameter(argc, argv, "height")) != NULL) {
         height = atoi(ptr);
     }
-    if ((ptr = args_get_long_option_parameter(argc, argv, "iterations")) != NULL)
-    {
+    if ((ptr = args_get_long_option_parameter(argc, argv, "iterations")) != NULL) {
         max_it = atoi(ptr);
     }
 
     log_info_fmt("width=%d, height=%d, iterations=%d", width, height, max_it);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int res = exdev_base_init();
     if (res) {
         log_warning_fmt("could no init exdev base=%d", res);
@@ -75,72 +69,57 @@ int main(int argc, char **argv)
 
     framebuffer_init(&fb, width, height);
     julia_init(&julia);
-    julia.max_it=max_it;
+    julia.max_it = max_it;
     julia_paint(&julia, &fb);
 
     window = window_create(width, height, "Julia", FS_NO);
     window_fill(window, &fb);
 
-    while (!close_event)
-    {
+    while (!close_event) {
         const int numKeyEvents = window_poll_events(window, &close_event, &events, 1);
-        if (numKeyEvents > 0)
-        {
-            if (events.event == KEY_EVENT_PRESSED)
-            {
-                if (events.type == KEY_TYPE_ESC)
-                {
+        if (numKeyEvents > 0) {
+            if (events.event == KEY_EVENT_PRESSED) {
+                if (events.type == KEY_TYPE_ESC) {
                     close_event = 1;
-                }
-                else if (events.type == KEY_TYPE_UP)
-                {
+                } else if (events.type == KEY_TYPE_UP) {
                     julia_move_up(&julia);
                     julia_paint(&julia, &fb);
                     window_fill(window, &fb);
-                }
-                else if (events.type == KEY_TYPE_DOWN)
-                {
+                } else if (events.type == KEY_TYPE_DOWN) {
                     julia_move_down(&julia);
                     julia_paint(&julia, &fb);
                     window_fill(window, &fb);
-                }
-                else if (events.type == KEY_TYPE_LEFT)
-                {
+                } else if (events.type == KEY_TYPE_LEFT) {
                     julia_move_left(&julia);
                     julia_paint(&julia, &fb);
                     window_fill(window, &fb);
-                }
-                else if (events.type == KEY_TYPE_RIGHT)
-                {
+                } else if (events.type == KEY_TYPE_RIGHT) {
                     julia_move_right(&julia);
                     julia_paint(&julia, &fb);
                     window_fill(window, &fb);
-                }
-                else if (events.type == KEY_TYPE_CODE)
-                {
-                    switch (events.code)
-                    {
-                    case 'q':
-                        close_event = 1;
-                        break;
-                    case '+':
-                        julia_move_zoom_in(&julia);
-                        julia_paint(&julia, &fb);
-                        window_fill(window, &fb);
-                        break;
-                    case '-':
-                        julia_move_zoom_out(&julia);
-                        julia_paint(&julia, &fb);
-                        window_fill(window, &fb);
-                        break;
-                    case 'r':
-                        julia_init(&julia);
-                        julia_paint(&julia, &fb);
-                        window_fill(window, &fb);
-                        break;
-                    case 's':
-                        framebuffer_save(&fb, IMAGE_PATH);
-                        log_info_fmt("saved image to:%s", IMAGE_PATH);
+                } else if (events.type == KEY_TYPE_CODE) {
+                    switch (events.code) {
+                        case 'q':
+                            close_event = 1;
+                            break;
+                        case '+':
+                            julia_move_zoom_in(&julia);
+                            julia_paint(&julia, &fb);
+                            window_fill(window, &fb);
+                            break;
+                        case '-':
+                            julia_move_zoom_out(&julia);
+                            julia_paint(&julia, &fb);
+                            window_fill(window, &fb);
+                            break;
+                        case 'r':
+                            julia_init(&julia);
+                            julia_paint(&julia, &fb);
+                            window_fill(window, &fb);
+                            break;
+                        case 's':
+                            framebuffer_save(&fb, IMAGE_PATH);
+                            log_info_fmt("saved image to:%s", IMAGE_PATH);
                     }
                 }
             }
