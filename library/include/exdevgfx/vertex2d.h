bd876b15df4efa51a210c6d66ab59f532fd337ca
Andre Geisler <andre@exdev.de>
2024-10-13 10:08:48 +0200

    add vertex2d_rotate

--- library/include/exdevgfx/vertex2d.h
+++ library/include/exdevgfx/vertex2d.h
@@ -14,4 +14,7 @@ void vertex2d_set(Vertex2d_t v, float x, float y);
 void vertex2d_copy(const Vertex2d_t src, Vertex2d_t dst);
 
 float vertex2d_cross(const Vertex2d_t a, const Vertex2d_t b);
+
+void vertex2d_rotate(const Vertex2d_t v, Vertex2d_t d, float a);
+
 #endif //EXDEVGFX2_VERTEX2D_H
