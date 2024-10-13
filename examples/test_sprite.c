b169ef9c9b6546bbdee4cf518ba84215198c183f
Andre Geisler <andre@exdev.de>
2024-10-13 10:08:47 +0200

    fixed draw rotated image

--- examples/test_sprite.c
+++ examples/test_sprite.c
@@ -2,24 +2,29 @@
 #include <exdevgfx/framebuffer_8bit.h>
 #include <exdevgfx/window.h>
 #include <exdevgfx/logger.h>
+
 #define WIDTH 640
 #define HEIGHT 480
-#define STEP 0.1f
+#define STEP_SCALE 0.1f
+#define STEP_ROTATE 5.0f
 
-static void paint(Window_t *window, Framebuffer8Bit_t *offscreen, Framebuffer8Bit_t *sprite, const float scale) {
+static void paint(Window_t *window, Framebuffer8Bit_t *offscreen, Framebuffer8Bit_t *sprite, const float scale, const float rotate) {
+    (void) rotate;
     // calc center
-    const int x = (int) (((float) offscreen->width - (float) sprite->width * scale) * 0.5f);
-    const int y = (int) (((float) offscreen->height - (float) sprite->height * scale) * 0.5f);
 
     // clear buffer + draw
     framebuffer_8bit_fill(offscreen, 0);
-    framebuffer_8bit_draw_framebuffer_scaled(offscreen, x, y, sprite, scale);
+    // render scale
+    framebuffer_8bit_draw_framebuffer_scaled(offscreen, WIDTH * 0.33f, HEIGHT * 0.5f, sprite, scale);
+    // render rotate
+    framebuffer_8bit_draw_framebuffer_rotated(offscreen, WIDTH * 0.66f, HEIGHT * 0.5f, sprite, rotate);
     window_fill_8bit(window, offscreen);
 }
 
 int main() {
     // variables
     float scale = 1.0f;
+    float rotate = 0.0f;
     Framebuffer8Bit_t offscreen;
     Framebuffer8Bit_t sprite;
     Palette8Bit_t palette;
@@ -27,7 +32,8 @@ int main() {
     char close_event = 0;
     KeyEvent_t keyEvent;
     int eventCount = 0;
-    float step = 0.0f;
+    float step_scale = 0.0f;
+    float step_rotate = 0.0f;
 
     // init
     exdev_base_init();
@@ -40,7 +46,7 @@ int main() {
         log_warning("could not read palette");
         return 2;
     }
-    if(!(window = window_create(WIDTH, HEIGHT, "Test Window", 0))){
+    if (!(window = window_create(WIDTH, HEIGHT, "Test Window", 0))) {
         log_warning("could not create window");
         return 3;
     }
@@ -49,26 +55,35 @@ int main() {
     // loop events and render
     while (!close_event) {
         // render
-        paint(window, &offscreen, &sprite, scale);
+        paint(window, &offscreen, &sprite, scale, rotate);
 
         // handle events
         eventCount = window_poll_events(window, &close_event, &keyEvent, 1);
         if (eventCount && keyEvent.event == KEY_EVENT_PRESSED && keyEvent.type == KEY_TYPE_ESC) {
             close_event = 1;
+        } else if (eventCount && keyEvent.event == KEY_EVENT_PRESSED && keyEvent.type == KEY_TYPE_LEFT) {
+            step_rotate -= STEP_ROTATE;
+        } else if (eventCount && keyEvent.event == KEY_EVENT_PRESSED && keyEvent.type == KEY_TYPE_RIGHT) {
+            step_rotate += STEP_ROTATE;
+        } else if (eventCount && keyEvent.event == KEY_EVENT_RELEASED && keyEvent.type == KEY_TYPE_LEFT) {
+            step_rotate = 0.0f;
+        } else if (eventCount && keyEvent.event == KEY_EVENT_RELEASED && keyEvent.type == KEY_TYPE_RIGHT) {
+            step_rotate = 0.0f;
         } else if (eventCount && keyEvent.event == KEY_EVENT_PRESSED && keyEvent.type == KEY_TYPE_CODE &&
                    keyEvent.code == '-') {
-            step = -STEP;
+            step_scale = -STEP_SCALE;
         } else if (eventCount && keyEvent.event == KEY_EVENT_RELEASED && keyEvent.type == KEY_TYPE_CODE &&
                    keyEvent.code == '-') {
-            step = 0.0f;
+            step_scale = 0.0f;
         } else if (eventCount && keyEvent.event == KEY_EVENT_PRESSED && keyEvent.type == KEY_TYPE_CODE &&
                    keyEvent.code == '+') {
-            step = STEP;
+            step_scale = STEP_SCALE;
         } else if (eventCount && keyEvent.event == KEY_EVENT_RELEASED && keyEvent.type == KEY_TYPE_CODE &&
                    keyEvent.code == '+') {
-            step = 0.0f;
+            step_scale = 0.0f;
         }
-        scale += step;
+        scale += step_scale;
+        rotate += step_rotate;
     }
 
     // deinit
