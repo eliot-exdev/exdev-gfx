126b2f185bc1e04c9b9be79bf07485c43104c603
Andre Geisler <andre@exdev.de>
2024-10-13 10:08:48 +0200

    implemented selection of easing function

--- examples/test_sprite.c
+++ examples/test_sprite.c
@@ -1,3 +1,5 @@
+#define EXDEVGFX2_LOG_LEVEL 2
+
 #include <exdevgfx/exdev_base.h>
 #include <exdevgfx/framebuffer_8bit.h>
 #include <exdevgfx/window.h>
@@ -5,15 +7,33 @@
 #include <exdevgfx/helper.h>
 
 #include <easing/Back.h>
+#include <easing/Bounce.h>
+#include <easing/Circ.h>
+#include <easing/Cubic.h>
+#include <easing/Elastic.h>
+#include <easing/Expo.h>
+#include <easing/Linear.h>
+#include <easing/Quad.h>
+#include <easing/Quart.h>
+#include <easing/Quint.h>
+#include <easing/Sine.h>
 
 #define WIDTH 640
 #define HEIGHT 480
 #define DURATION 4000 // in ms
+#define NUM_FUNCTIONS 11
 
 static Vertex2d_t posSprite1 = {WIDTH * 0.33f, HEIGHT * 0.5f};
 static Vertex2d_t posSprite2 = {WIDTH * 0.66f, HEIGHT * 0.5f};
 
-static void paint(Window_t *window, Framebuffer8Bit_t *offscreen, Framebuffer8Bit_t *sprite, const float scale, const float rotate) {
+typedef float (*penner_functions_t)(float t, float b, float c, float d);
+
+static penner_functions_t functions[NUM_FUNCTIONS];
+
+static int functions_index = 0;
+
+static void paint(Window_t *window, Framebuffer8Bit_t *offscreen, Framebuffer8Bit_t *sprite, const float scale,
+                  const float rotate) {
     // clear buffer + draw
     framebuffer_8bit_fill(offscreen, 31);
     // render scale
@@ -33,6 +53,19 @@ int main() {
     Window_t *window = NULL;
     char close_event = 0;
     Event_t event;
+
+    // functions
+    functions[0] = easing_back_easeInOut;
+    functions[1] = easing_bounce_easeInOut;
+    functions[2] = easing_circ_easeInOut;
+    functions[3] = easing_cubic_easeInOut;
+    functions[4] = easing_elastic_easeIn;
+    functions[5] = easing_expo_easeIn;
+    functions[6] = easing_linear_easeIn;
+    functions[7] = easing_quad_easeIn;
+    functions[8] = easing_quart_easeIn;
+    functions[9] = easing_quint_easeIn;
+    functions[10] = easing_sine_easeIn;
 
     // init
     exdev_base_init();
@@ -61,8 +94,16 @@ int main() {
         // handle events
         window_poll_events(window, &close_event, &event, 1);
 
-        if (event.type == EVENT_KEY && event.key_event.event == KEY_EVENT_PRESSED && event.key_event.key == KEY_TYPE_ESC) {
-            close_event = 1;
+        if (event.type == EVENT_KEY && event.key_event.event == KEY_EVENT_PRESSED) {
+            if (event.key_event.key == KEY_TYPE_ESC) {
+                close_event = 1;
+            } else if (event.key_event.code == 's') {
+                ++functions_index;
+                if (functions_index == NUM_FUNCTIONS) {
+                    functions_index = 0;
+                }
+                log_info_fmt("switched to easing function=%d", functions_index);
+            }
         }
 
         if (event.type == EVENT_MOUSE && event.mouse_event.event == MOUSE_EVENT_BUTTON_PRESSED) {
@@ -87,11 +128,11 @@ int main() {
             }
         }
         if (in) {
-            scale = easing_back_easeInOut((float) t, 1.0f, 2.0f, DURATION);
-            rotate = easing_back_easeInOut((float) t, 0.0f, 360.f, DURATION);
+            scale = functions[functions_index]((float) t, 1.0f, 2.0f, DURATION);
+            rotate = functions[functions_index]((float) t, 0.0f, 360.f, DURATION);
         } else {
-            scale = easing_back_easeInOut((float) t, 3.0f, -2.0f, DURATION);
-            rotate = easing_back_easeInOut((float) t, 360.0f, -360.0f, DURATION);
+            scale = functions[functions_index]((float) t, 3.0f, -2.0f, DURATION);
+            rotate = functions[functions_index]((float) t, 360.0f, -360.0f, DURATION);
         }
     }
 
