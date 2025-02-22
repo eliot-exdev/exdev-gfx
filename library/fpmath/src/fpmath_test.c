16097773cba4b50c65432f1981838fd8c34fc341
Andre Geisler <andre@exdev.de>
2025-02-22 08:05:15 +0100

    initial import of fpmath

---
+++ library/fpmath/src/fpmath_test.c
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2025 Andre Geisler (andre@exdev.de)
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+    printf("begin test fpmath ...\n");
+
+    printf("... end test fpmath!\n");
+    return 0;
+}
