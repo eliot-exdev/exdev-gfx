45391a282732b7545edd4b9496f4dba233d49fc4
Andre Geisler <andre@exdev.de>
2025-02-27 11:17:18 +0100

    fixed timestamp calculation

--- library/src_amiga/helper_amiga.c
+++ library/src_amiga/helper_amiga.c
@@ -11,9 +11,5 @@ static struct timeval a;
 
 EXDEV_TIMESTAMP now() {
     GetSysTime(&a);
-#ifdef EXDEV_FP_MATH
-    return a.tv_secs * 1000 + exdev_fp_to_int(exdev_fp_div(exdev_int_to_fp(a.tv_micro), exdev_int_to_fp(1000)));
-#else
-    return a.tv_secs*1000 + a.tv_micro/1000;
-#endif
+    return a.tv_secs * 1000 + a.tv_micro/1000;
 }
