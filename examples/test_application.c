1210a95873ce8d1066e8693aac9f66a1b24fcb95
Andre Geisler <andre@exdev.de>
2025-12-17 12:02:50 +0100

    add missing init/deinit

--- examples/test_application.c
+++ examples/test_application.c
@@ -1,10 +1,15 @@
 #include <exdevgfx/ui/ui.h>
+#include <exdevgfx/exdev_base.h>
 
 int main() {
+    exdev_base_init();
+
     Application_t app;
     application_init(&app, "test", 640, 480);
     application_run(&app, 40);
     application_destroy(&app);
+
+    exdev_base_deinit();
 
     return 0;
 }
