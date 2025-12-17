08414ec8fd5e7b5a74339f23824d803fa731376e
Andre Geisler <andre@exdev.de>
2025-12-17 12:02:50 +0100

    add comments

--- examples/test_application.c
+++ examples/test_application.c
@@ -7,7 +7,7 @@
 
 #define WIDTH 640
 #define HEIGHT 480
-#define UPDATE_INTERVAL 50// ms
+#define UPDATE_INTERVAL 50 // ms
 
 void left_update(UIComponent_t *self, const exdev_timestamp_t ms, const Event_t *events, const int num_events) {
     ui_component_update(self, ms, events, num_events);// call update of base class
@@ -78,11 +78,11 @@ int main() {
     // application
     Application_t app;
     application_init(&app, WIDTH, HEIGHT);
-    palette_8bit_read_from_dat(&app.palette, "assets/amiga_logo_8bit.pal");
+    palette_8bit_read_from_dat(&app.palette, "assets/amiga_logo_8bit.pal"); // read palette from file
 
     // left component
     UIComponent_t *left = ui_component_create(2, 2, 538, 476);
-    left->functions.update_func = (void (*)(void *, exdev_timestamp_t, const Event_t *, int)) &left_update;
+    left->functions.update_func = (void (*)(void *, exdev_timestamp_t, const Event_t *, int)) &left_update; // custom event handling
     ui_component_connect(&app.root, left);
 
     // icon
@@ -91,8 +91,8 @@ int main() {
 
     // right component
     UIComponent_t *right = ui_component_create(542, 2, 96, 476);
-    right->functions.paint_func = (int (*)(void *, Framebuffer8Bit_t *)) &right_paint;
-    right->functions.update_func = (void (*)(void *, exdev_timestamp_t, const Event_t *, int)) &right_update;
+    right->functions.paint_func = (int (*)(void *, Framebuffer8Bit_t *)) &right_paint; // custom paint
+    right->functions.update_func = (void (*)(void *, exdev_timestamp_t, const Event_t *, int)) &right_update; // custom event handling
     ui_component_connect(&app.root, right);
 
     // run
